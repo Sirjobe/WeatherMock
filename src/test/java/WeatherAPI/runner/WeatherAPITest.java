8721f12a5803cce1eac4dbb9cc66638b882445cf
Sirjobe <kalenyuk1998@gmail.com>
2025-08-03 17:13:59 +0300

    This project implements BDD-style API testing for a weather service using WireMock for stubbing backend responses, Cucumber for test scenarios, and Allure for detailed test reporting with rich attachments.

---
+++ src/test/java/WeatherAPI/runner/WeatherAPITest.java
@@ -0,0 +1,19 @@
+package WeatherAPI.runner;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameters({
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "WeatherAPI.step"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+                value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, html:target/cucumber-report/cucumber.html"),
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@positive or @negative")
+})
+@SuiteDisplayName("Weather API Cucumber Tests")
+@ExcludeEngines("junit-jupiter") // Добавьте эту строку
+public class WeatherAPITest {
+}
